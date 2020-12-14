de1c090aa19ccdea30e7e2b8406737d4202bfa06
PabloCanelas <54004206+PabloCanelas@users.noreply.github.com>
2020-12-14 13:42:35 -0400

    Update IEstimateStrategy.js

--- Microservice3/Strategy/IEstimateStrategy.js
+++ Microservice3/Strategy/IEstimateStrategy.js
@@ -1,7 +1,6 @@
 class IEstimateStrategy {
   constructor() {
     if (this.estimate === undefined) {
-      // or maybe test typeof this.method === "function"
       throw new TypeError("Must override method");
     }
   }
